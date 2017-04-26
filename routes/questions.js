782681ebcfe84479e262937826b9659a55ee956c
Amin Rajabi <amin7x6@gmail.com>
2017-04-26 15:35:58 -0700

    Add answer list in question show

--- routes/questions.js
+++ routes/questions.js
@@ -49,10 +49,22 @@ router.get('/:id', function (req, res){
 
   Question
     .findById(id)
-    .then(function (question){
-      res.render('questions/show', {question: question})
+    .then(function (question) {
+      return Promise.all([
+        question,
+         question.getAnswers({order: [['createdAt', 'DESC']]})
+       ])
+    })
+    // NEW! Array Destructuring
+    // const [first, second, ...rest] = [1, 2, 3, 4, 5, 6]
+    // first === 1; second === 2, rest === [3, 4, 5. 6]
+    // ð can also be done with function arguments ð
+    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Destructuring_assignment
+    .then(function ([question, answers]) {
+      res.render('questions/show', {question: question, answers: answers})
     })
 })
+
 
 // URL: /questions/:questionId/answers VERB: All of them!
 router.use('/:questionId/answers', answers);
